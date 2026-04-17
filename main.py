84f3175e6285d9a06fc288914eb737abc207ff17
Alikhan <alikhanmoldabaev75@gmail.com>
2026-04-17 17:19:57 +0500

    update

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello, World! How are you ?")
+
+git init
+git add .
+git commit -m "init"
+git remote add origin https://github.com/Cowspump/For_repair_website.git
+git branch -M main
+git push -u origin main
